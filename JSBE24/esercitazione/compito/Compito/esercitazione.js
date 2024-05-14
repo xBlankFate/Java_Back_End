2a016425c2e998e862620158875d7e5040d3038d
xBlankFate <165818657+xBlankFate@users.noreply.github.com>
2024-05-14 17:51:14 +0200

    Add files via upload

---
+++ JSBE24/esercitazione/compito/Compito/esercitazione.js
@@ -0,0 +1,7 @@
+let numero1 = 7;
+let numero2 = 55;
+let numero3 = numero1 + numero2;
+console.log("la somma tra: "+ numero1 + " e " + numero2 + " è " + numero3);
+
+let differenza = numero2 - numero1;
+console.log("la differenza tra: " + numero2 + " e " + numero1 + " è " + differenza);
